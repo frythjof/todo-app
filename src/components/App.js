2d4e18a87adccd2e665169b4b058450aa66566f8
Kai <kai@MacBook-Pro-10.local>
2018-11-15 14:43:12 +0100

    Add routes and style navigation

--- src/components/App.js
+++ src/components/App.js
@@ -1,126 +1,52 @@
-import React, { Component } from 'react'
-import Input from './Input'
-import Todo from './Todo'
-import Separator from './Separator'
-import ProgressBar from './ProgressBar'
-import ToggleButton from './ToggleButton'
-import uid from 'uid'
-//import './App.css'
+import React from 'react'
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom'
 import styled from 'styled-components'
+import Home from './Home'
+import Config from './Config'
 
-export const Container = styled.div`
+const Wrapper = styled.div`
   display: grid;
-  grid-auto-rows: min-content;
-  padding: 0 10px;
+  grid-template-rows: auto 50px;
   height: 100vh;
-  background-color: darkgray;
 `
 
-const TodoList = styled.ul`
-  display: grid;
-  grid-gap: 10px;
+const NavWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  grid-gap: 5px;
+  background-color: darkgrey;
+  border: 2px solid black;
+
+  a:any-link {
+    text-decoration: none;
+    color: white;
+    font-weight: bold;
+    font-size: 22px;
+  }
+
+  .active {
+    background-color: deeppink;
+  }
 `
 
-class App extends Component {
-  state = {
-    todos: [
-      // this.load()
-      { text: 'Geschenk1', done: false, id: 'kjhwerq' },
-      { text: 'Geschenk2', done: false, id: 'kjhwerz' },
-      { text: 'Geschenk3', done: false, id: 'kjhwert' },
-      { text: 'Geschenk4', done: false, id: 'kjhwers' },
-      { text: 'Geschenk5', done: false, id: 'kjhwerd' },
-      { text: 'Geschenk6', done: false, id: 'kjhwerf' },
-      { text: 'Geschenk7', done: false, id: 'kjhwerg' }
-    ]
-  }
-
-  render() {
-    this.save()
-    return (
-      <Container>
-        <Input onEnter={this.addTodo} placeholder="Insert your todo here" />
-        <Separator text="TO DO" />
-        <TodoList>{this.renderOpenTodos()}</TodoList>
-        <Separator text="DONE" />
-        <TodoList>{this.renderDoneTodos()}</TodoList>
-        <ProgressBar
-          text="Progress: "
-          progress={
-            (this.renderDoneTodos().length / this.state.todos.length) * 100
-          }
-        />
-        <ToggleButton
-          defaultText="Foo"
-          altText="Bar"
-          isDefault={true}
-          onClick={() => console.log('Hello')}
-        />
-      </Container>
-    )
-  }
-
-  renderOpenTodos() {
-    return this.state.todos
-      .filter(todo => !todo.done)
-      .map(this.renderSingleTodo)
-  }
-
-  renderDoneTodos() {
-    return this.state.todos.filter(todo => todo.done).map(this.renderSingleTodo)
-  }
-
-  renderSingleTodo = todo => (
-    <Todo
-      key={todo.id}
-      text={todo.text}
-      done={todo.done}
-      onToggle={() => this.toggleTodo(todo.id)}
-      onDelete={() => this.deleteTodo(todo.id)}
-    />
-  )
-
-  addTodo = text => {
-    this.setState({
-      todos: [{ text, done: false, id: uid() }, ...this.state.todos]
-    })
-  }
-
-  toggleTodo = id => {
-    const { todos } = this.state
-    const index = todos.findIndex(t => t.id === id)
-    const newTodos = [
-      ...todos.slice(0, index),
-      { ...todos[index], done: !todos[index].done },
-      ...todos.slice(index + 1)
-    ]
-
-    this.setState({
-      todos: newTodos
-    })
-  }
-
-  deleteTodo = id => {
-    const { todos } = this.state
-    const index = todos.findIndex(t => t.id === id)
-    const newTodos = [...todos.slice(0, index), ...todos.slice(index + 1)]
-
-    this.setState({
-      todos: newTodos
-    })
-  }
-
-  save() {
-    localStorage.setItem('todo-app--todos', JSON.stringify(this.state.todos))
-  }
-
-  load() {
-    try {
-      return JSON.parse(localStorage.getItem('todo-app--todos')) || []
-    } catch (err) {
-      return []
-    }
-  }
-}
+const App = () => (
+  <Router>
+    <Wrapper>
+      <div>
+        <Route path="/" exact component={Home} />
+        <Route path="/config/" component={Config} />
+      </div>
+      <NavWrapper>
+        <NavLink exact activeClassName="active" to="/">
+          Home
+        </NavLink>
+        <NavLink activeClassName="active" to="/config/">
+          Settings
+        </NavLink>
+      </NavWrapper>
+    </Wrapper>
+  </Router>
+)
 
 export default App
---
+++ src/components/Home.js
@@ -0,0 +1,126 @@
+import React, { Component } from 'react'
+import Input from './Input'
+import Todo from './Todo'
+import Separator from './Separator'
+import ProgressBar from './ProgressBar'
+import ToggleButton from './ToggleButton'
+import uid from 'uid'
+//import './Home.css'
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  display: grid;
+  grid-auto-rows: min-content;
+  padding: 0 10px;
+  background-color: darkgray;
+`
+// height: 100vh;
+
+const TodoList = styled.ul`
+  display: grid;
+  grid-gap: 10px;
+`
+
+class Home extends Component {
+  state = {
+    todos: [
+      // this.load()
+      { text: 'Geschenk1', done: false, id: 'kjhwerq' },
+      { text: 'Geschenk2', done: false, id: 'kjhwerz' },
+      { text: 'Geschenk3', done: false, id: 'kjhwert' },
+      { text: 'Geschenk4', done: false, id: 'kjhwers' },
+      { text: 'Geschenk5', done: false, id: 'kjhwerd' },
+      { text: 'Geschenk6', done: false, id: 'kjhwerf' },
+      { text: 'Geschenk7', done: false, id: 'kjhwerg' }
+    ]
+  }
+
+  render() {
+    this.save()
+    return (
+      <Container>
+        <Input onEnter={this.addTodo} placeholder="Insert your todo here" />
+        <Separator text="TO DO" />
+        <TodoList>{this.renderOpenTodos()}</TodoList>
+        <Separator text="DONE" />
+        <TodoList>{this.renderDoneTodos()}</TodoList>
+        <ProgressBar
+          text="Progress: "
+          progress={
+            (this.renderDoneTodos().length / this.state.todos.length) * 100
+          }
+        />
+        <ToggleButton
+          defaultText="Foo"
+          altText="Bar"
+          isDefault={true}
+          onClick={() => console.log('Hello')}
+        />
+      </Container>
+    )
+  }
+
+  renderOpenTodos() {
+    return this.state.todos
+      .filter(todo => !todo.done)
+      .map(this.renderSingleTodo)
+  }
+
+  renderDoneTodos() {
+    return this.state.todos.filter(todo => todo.done).map(this.renderSingleTodo)
+  }
+
+  renderSingleTodo = todo => (
+    <Todo
+      key={todo.id}
+      text={todo.text}
+      done={todo.done}
+      onToggle={() => this.toggleTodo(todo.id)}
+      onDelete={() => this.deleteTodo(todo.id)}
+    />
+  )
+
+  addTodo = text => {
+    this.setState({
+      todos: [{ text, done: false, id: uid() }, ...this.state.todos]
+    })
+  }
+
+  toggleTodo = id => {
+    const { todos } = this.state
+    const index = todos.findIndex(t => t.id === id)
+    const newTodos = [
+      ...todos.slice(0, index),
+      { ...todos[index], done: !todos[index].done },
+      ...todos.slice(index + 1)
+    ]
+
+    this.setState({
+      todos: newTodos
+    })
+  }
+
+  deleteTodo = id => {
+    const { todos } = this.state
+    const index = todos.findIndex(t => t.id === id)
+    const newTodos = [...todos.slice(0, index), ...todos.slice(index + 1)]
+
+    this.setState({
+      todos: newTodos
+    })
+  }
+
+  save() {
+    localStorage.setItem('todo-app--todos', JSON.stringify(this.state.todos))
+  }
+
+  load() {
+    try {
+      return JSON.parse(localStorage.getItem('todo-app--todos')) || []
+    } catch (err) {
+      return []
+    }
+  }
+}
+
+export default Home
